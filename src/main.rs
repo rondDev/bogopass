aa09b2898f310fa23bc7484c915ee6b95ac121fc
rond <contact@rond.cc>
2024-03-19 23:57:02 +0100

    perf: remove multithreading

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,7 @@ use std::{
     thread,
 };
 
-use randomizer::Randomizer;
-
+use rand::Rng;
 fn main() {
     let ascii_letters = String::from_utf8((b'a'..=b'z').chain(b'A'..=b'Z').collect()).unwrap();
     let punctuation = String::from_utf8(
@@ -46,43 +45,37 @@ fn main() {
 
     let chars = string_to_static_str(char_set);
 
-    println!("Amount of threads?");
-    let thread_count = read_string().trim().parse::<i32>().unwrap();
-
     let num = Arc::new(AtomicI32::new(0));
 
     let mut solved = false;
 
-    let mut threads = vec![];
-
     let output_password = Arc::new(Mutex::new(String::from("")));
     let outp = output_password.clone();
 
-    for _ in 0..thread_count {
-        let n = Arc::clone(&num);
-        let pass = Arc::clone(&output_password);
-        threads.push(thread::spawn(move || loop {
-            if solved {
-                break;
-            }
-            let p = Randomizer::new(
-                letter_count.clone() + symbol_count + number_count,
-                Some(chars),
-            )
-            .string()
-            .unwrap();
-            n.fetch_add(1, Ordering::SeqCst);
-            if check_pass(&p, letter_count, symbol_count, number_count) {
-                solved = true;
-                *pass.lock().unwrap() = p.to_string();
-            }
-            // println!("{} on try {}", p, n.load(Ordering::SeqCst));
-        }));
-    }
+    let n = Arc::clone(&num);
+    let pass = Arc::clone(&output_password);
+    let thread = thread::spawn(move || loop {
+        if solved {
+            break;
+        }
+        let mut rng = rand::thread_rng();
+        let mut o = String::from("");
+        for _ in 0..(letter_count + symbol_count + number_count) {
+            o.push(
+                chars
+                    .chars()
+                    .nth(rng.gen_range(0..chars.len()) as usize)
+                    .unwrap(),
+            );
+        }
+        n.fetch_add(1, Ordering::SeqCst);
+        if check_pass(&o, letter_count, symbol_count, number_count) {
+            solved = true;
+            *pass.lock().unwrap() = o.to_string();
+        }
+    });
 
-    for thread in threads {
-        let _ = thread.join();
-    }
+    let _ = thread.join();
     println!(
         "Your password is: {} and it took {:?} tries.",
         outp.lock().unwrap(),
@@ -96,19 +89,15 @@ fn check_pass(pass: &str, letters: usize, symbols: usize, numbers: usize) -> boo
     let mut number_count = 0;
     for k in pass.chars() {
         let c = k as u8;
-        if (c >= 65 && c <= 90) || (c >= 97 && c <= 122) {
+        if c.is_ascii_alphabetic() {
             letter_count += 1;
             continue;
         }
-        if (c >= 33 && c <= 47)
-            || (c >= 58 && c <= 64)
-            || (c >= 91 && c <= 96)
-            || (c >= 123 && c <= 126)
-        {
+        if c.is_ascii_punctuation() {
             symbol_count += 1;
             continue;
         }
-        if c >= 48 && c <= 57 {
+        if c.is_ascii_digit() {
             number_count += 1;
             continue;
         }
